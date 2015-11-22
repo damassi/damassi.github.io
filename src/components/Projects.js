7a9726e84e77f43328c14218837c376e517b1abe
Christopher Pappas <damassi.pappas@gmail.com>
2015-11-21 19:13:29 -0800

    Updated apps

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -56,9 +56,28 @@ function getProjects() {
   return {
     'posts': [
       {
+        'url': '',
+        'title': 'Assignment Desk',
+        'content': "<p><img src='/assets/images/ad/ad-0.png' width='700' height='auto' /></p><p><img src='/assets/images/ad/ad-1.png' width='700' height='auto' /></p><p><img src='/assets/images/ad/ad-2.png' width='700' height='auto' /></p><p><img src='/assets/images/ad/ad-3.png' width='700' height='auto' /></p><p><img src='/assets/images/ad/ad-4.png' width='700' height='auto' /></p>",
+        'tags': [
+          'ES6+',
+          'Functional',
+          'Framework',
+          'Fullstack',
+          'Node.js',
+          'Restify',
+          'React',
+          'React Router',
+          'Redux',
+          'Redux Form',
+          'Webpack'
+        ],
+        'description': "Built under extreme time constraints, Assignment Desk is part one of a series of software applications for Time's Digital Asset Management team. As the lead developer and architect on the project, I was responsible for designing and developing a scalable React-based framework, as well as the supporting backend API service layer. The project was built entirely in the functional style, with Redux serving as my state management library of choice."
+      },
+      {
         'url': 'http://www.kindlingapp.com',
         'title': 'Kindling',
-        'content': '<p><img class=\'673\' alt=\'1\' src=\'/assets/images/2015/kindling/1.png\' width=\'700\' height=\'auto\' /></p>\n<p><img class=\'674\' alt=\'2\' src=\'/assets/images/2015/kindling/2.png\' width=\'700\' height=\'auto\' /></p>\n<p><img class=\'676\' alt=\'4\' src=\'/assets/images/2015/kindling/3.png\' width=\'700\' height=\'auto\' /></p>\n<p><img class=\'676\' alt=\'4\' src=\'/assets/images/2015/kindling/4.png\' width=\'700\' height=\'auto\' /></p>\n',
+        'content': "<p><img src='/assets/images/2015/kindling/1.png' width='700' height='auto' /></p> <p><img src='/assets/images/2015/kindling/2.png' width='700' height='auto' /></p> <p><img src='/assets/images/2015/kindling/3.png' width='700' height='auto' /></p> <p><img src='/assets/images/2015/kindling/4.png' width='700' height='auto' /></p> ",
         'tags': [
           'backbone',
           'react',
@@ -70,7 +89,7 @@ function getProjects() {
       {
         'url': 'https://itunes.apple.com/us/app/amazon-local-register-amazons/id825947720?mt=8',
         'title': 'Amazon Local Register Mobile App',
-        'content': '<p><img class=\'673\' alt=\'1\' src=\'/assets/images/2014/amz/1.jpg\' width=\'700\' height=\'359\' /></p>\n<p><img class=\'674\' alt=\'2\' src=\'/assets/images/2014/amz/2.jpg\' width=\'700\' height=\'357\' /></p>\n<p><img class=\'676\' alt=\'4\' src=\'/assets/images/2014/amz/3.jpg\' width=\'700\' height=\'333\' /></p>\n',
+        'content': "<p><img src='/assets/images/2014/amz/1.jpg' width='700' height='359' /></p> <p><img src='/assets/images/2014/amz/2.jpg' width='700' height='357' /></p> <p><img src='/assets/images/2014/amz/3.jpg' width='700' height='333' /></p> ",
         'tags': [
           'backbone',
           'responsive',
@@ -79,9 +98,35 @@ function getProjects() {
         'description': "Amazon Local Register is Amazon's entry into the Point of Sale marketplace.  As one of three core JavaScript developers responsible for architecting and building out the customer-facing portion of the app and store, I touched nearly every aspect of the code, from POC to final delivery."
       },
       {
+        'url': '',
+        'title': 'Sonifi',
+        'content': "<p><img src='/assets/images/sonifi/sonifi-1.png' width='700' height='auto' /></p>",
+        'tags': [
+          'Design',
+          'Flummox',
+          'Fullstack',
+          'MapBox',
+          'React',
+          'Ruby on Rails',
+          'Soundscapes',
+        ],
+        'description': "Sonifi is a proof-of-concept software application that allows users to create geographical \"SoundScapes\" by uploading audio and annotating their environment. As more and more sounds are added and positioned, users can zoom in and navigate in full 3-dimensional sound. I was responsible for design and development, and chose React.js for the frontend and Ruby on Rails for the backend."
+      },
+      {
+        'url': 'http://www.avvo.com/advisor',
+        'title': 'Avvo Advisor',
+        'content': "<p><img src='/assets/images/avvo/avvo-0.jpg' width='700' height='auto' /></p><p><img src='/assets/images/avvo/avvo-1.jpg' width='700' height='auto' /> ",
+        'tags': [
+          'Ruby On Rails',
+          'JavaScript',
+          'Twilio'
+        ],
+        'description': 'Avvo Advisor is a software application that connects people to instant (or almost instant) legal advice. Working in Ruby on Rails, I was a part of the team responsible for building the app.'
+      },
+      {
         'url': 'http://www.ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.com/',
         'title': 'Coke | MPC Ahhh Beat Maker App',
-        'content': '<p><img class=\'673\' alt=\'1\' src=\'/assets/images/2014/06/1.jpg\' width=\'700\' height=\'359\' /></p>\n<p><img class=\'674\' alt=\'2\' src=\'/assets/images/2014/06/2.jpg\' width=\'700\' height=\'357\' /></p>\n<p><img class=\'676\' alt=\'4\' src=\'/assets/images/2014/06/41.png\' width=\'700\' height=\'333\' /></p>\n',
+        'content': "<p><img src='/assets/images/2014/06/1.jpg' width='700' height='359' /></p> <p><img src='/assets/images/2014/06/2.jpg' width='700' height='357' /></p> <p><img src='/assets/images/2014/06/41.png' width='700' height='333' /></p> ",
         'tags': [
           'backbone',
           'parse',
@@ -93,18 +138,18 @@ function getProjects() {
       {
         'url': '',
         'title': 'Toyota | EShowroom',
-        'content': '<p><a href=\'assets/images/2013/07/toy-1.jpg\'><img class=\'452\' alt=\'toy-1\' src=\'/assets/images/2013/07/toy-1.jpg\' width=\'700\' height=\'611\' /><br />\n</a> <img class=\'453\' alt=\'toy-2\' src=\'/assets/images/2013/07/toy-2.jpg\' width=\'700\' height=\'611\' /><br />\n<img class=\'454\' alt=\'toy-3\' src=\'/assets/images/2013/07/toy-3.jpg\' width=\'700\' height=\'611\' /><br />\n<img class=\'455\' alt=\'toy-home\' src=\'/assets/images/2013/07/toy-home.jpg\' width=\'700\' height=\'611\' /><br />\n<img class=\'456\' alt=\'toy-profile\' src=\'/assets/images/2013/07/toy-profile.jpg\' width=\'700\' height=\'611\' /><br />\n<a href=\'assets/images/2013/07/toy-video.jpg\'><img class=\'457\' alt=\'toy-video\' src=\'/assets/images/2013/07/toy-video.jpg\' width=\'700\' height=\'611\' /><br />\n</a></p>\n',
+        'content': "<p><a href='assets/images/2013/07/toy-1.jpg'><img  alt='toy-1' src='/assets/images/2013/07/toy-1.jpg' width='700' height='611' /><br /> </a> <img  alt='toy-2' src='/assets/images/2013/07/toy-2.jpg' width='700' height='611' /><br /> <img  alt='toy-3' src='/assets/images/2013/07/toy-3.jpg' width='700' height='611' /><br /> <img  alt='toy-home' src='/assets/images/2013/07/toy-home.jpg' width='700' height='611' /><br /> <img  alt='toy-profile' src='/assets/images/2013/07/toy-profile.jpg' width='700' height='611' /><br /> <a href='assets/images/2013/07/toy-video.jpg'><img  alt='toy-video' src='/assets/images/2013/07/toy-video.jpg' width='700' height='611' /><br /> </a></p> ",
         'tags': [
           'backbone',
           'marionette',
           'less'
         ],
-        'description': "Toyota EShowroom was Toyota's attempt to modernize how thousands of dealerships across the world communicate with one another, and with their customers.  Not only was it a complete digital catalog of their cars and trucks, but also an internal social network which allowed managers, customer service representatives and other employees to communicate, share information and stay up to date with the latest offerings from Toyota.  \r\n\r\n<br/><br/>\r\n\r\nAs one of many JavaScript developers on the project, I was tasked with building out the main product pages and search, as well as refactoring, towards the end, many (many) thousands of lines of code for maintainability and stability reasons.  \r\n\r\n<br/><br/>\r\n\r\nThe project is currently only viewable internally by Toyota employees, but the screenshots to your left should give you a glimpse at the complexity of the task.  \r\n\r\n<br/><br/>\r\n\r\nThis was, by far, POP Agency's largest digital project, many years in the making."
+        'description': "Toyota EShowroom was Toyota's attempt to modernize how thousands of dealerships across the world communicate with one another, and with their customers.  Not only was it a complete digital catalog of their cars and trucks, but also an internal social network which allowed managers, customer service representatives and other employees to communicate, share information and stay up to date with the latest offerings from Toyota.  \r \r <br/><br/>\r \r As one of many JavaScript developers on the project, I was tasked with building out the main product pages and search, as well as refactoring, towards the end, many (many) thousands of lines of code for maintainability and stability reasons.  \r \r <br/><br/>\r \r The project is currently only viewable internally by Toyota employees, but the screenshots to your left should give you a glimpse at the complexity of the task.  \r \r <br/><br/>\r \r This was, by far, POP Agency's largest digital project, many years in the making."
       },
       {
         'url': '',
         'title': 'Nike+ | FuelBand SE Global Kiosk',
-        'content': '<p><img class=\'658\' alt=\'image\' src=\'/assets/images/2013/11/image.jpg\' width=\'960\' height=\'710\' /></p>\n<p><img class=\'653\' alt=\'Nike_b_1\' src=\'/assets/images/2013/11/Nike_b_1.png\' width=\'700\' height=\'394\' /></p>\n<p>&nbsp;</p>\n',
+        'content': "<p><img  alt='image' src='/assets/images/2013/11/image.jpg' width='960' height='710' /></p> <p><img  alt='Nike_b_1' src='/assets/images/2013/11/Nike_b_1.png' width='700' height='394' /></p> <p>&nbsp;</p> ",
         'tags': [
           'backbone',
           'sass'
@@ -114,7 +159,7 @@ function getProjects() {
       {
         'url': '',
         'title': 'Nike | Factory Store Kiosk',
-        'content': '<p><img class=\'489\' alt=\'nike-1\' src=\'/assets/images/2013/07/nike-1.jpg\' width=\'700\' height=\'1244\' /> <img class=\'490\' alt=\'nike-2\' src=\'/assets/images/2013/07/nike-2.jpg\' width=\'700\' height=\'1244\' /> <img class=\'491\' alt=\'nike-3\' src=\'/assets/images/2013/07/nike-3.jpg\' width=\'700\' height=\'1244\' /> <img class=\'492\' alt=\'nike-4\' src=\'/assets/images/2013/07/nike-4.jpg\' width=\'700\' height=\'1244\' /></p>\n<p><img class=\'648\' alt=\'nike-instore\' src=\'/assets/images/2013/07/nike-instore.jpg\' width=\'700\' height=\'941\' /></p>\n',
+        'content': "<p><img  alt='nike-1' src='/assets/images/2013/07/nike-1.jpg' width='700' height='1244' /> <img  alt='nike-2' src='/assets/images/2013/07/nike-2.jpg' width='700' height='1244' /> <img  alt='nike-3' src='/assets/images/2013/07/nike-3.jpg' width='700' height='1244' /> <img  alt='nike-4' src='/assets/images/2013/07/nike-4.jpg' width='700' height='1244' /></p> <p><img  alt='nike-instore' src='/assets/images/2013/07/nike-instore.jpg' width='700' height='941' /></p> ",
         'tags': [
           'animation',
           'backbone',
@@ -126,7 +171,7 @@ function getProjects() {
       {
         'url': '',
         'title': 'SHOWTIME ANYTIME',
-        'content': '<p><img class=\'121\' title=\'show_1\' alt=\'\' src=\'/assets/images/2012/03/show_1.jpg\' width=\'700\' height=\'420\' /></p>\n<p><img class=\'122\' title=\'sho_2\' alt=\'\' src=\'/assets/images/2012/03/sho_2.jpg\' width=\'700\' height=\'420\' /></p>\n<p><img class=\'123\' title=\'show_3\' alt=\'\' src=\'/assets/images/2012/03/show_3.jpg\' width=\'700\' height=\'420\' /></p>\n<p><img class=\'124\' title=\'sho_4\' alt=\'\' src=\'/assets/images/2012/03/sho_4.jpg\' width=\'700\' height=\'420\' /></p>\n',
+        'content': "<p><img  title='show_1' alt='' src='/assets/images/2012/03/show_1.jpg' width='700' height='420' /></p> <p><img  title='sho_2' alt='' src='/assets/images/2012/03/sho_2.jpg' width='700' height='420' /></p> <p><img  title='show_3' alt='' src='/assets/images/2012/03/show_3.jpg' width='700' height='420' /></p> <p><img  title='sho_4' alt='' src='/assets/images/2012/03/sho_4.jpg' width='700' height='420' /></p> ",
         'tags': [
           'AS3',
           'Brightcove Video API'
@@ -136,7 +181,7 @@ function getProjects() {
       {
         'url': '',
         'title': 'Motorola | Global Design Wiki',
-        'content': '<p><img class=\'97\' title=\'moto_6\' alt=\'\' src=\'/assets/images/2012/05/moto_6.jpg\' width=\'700\' height=\'420\' /></p>\n<p><img title=\'moto_3\' alt=\'\' src=\'/assets/images/2012/05/moto_3.jpg\' width=\'700\' height=\'420\' /></p>\n',
+        'content': "<p><img  title='moto_6' alt='' src='/assets/images/2012/05/moto_6.jpg' width='700' height='420' /></p> <p><img title='moto_3' alt='' src='/assets/images/2012/05/moto_3.jpg' width='700' height='420' /></p> ",
         'tags': [
           'Custom Plugin Developemnt',
           'JavaScript',
@@ -148,7 +193,7 @@ function getProjects() {
       {
         'url': '',
         'title': 'Quit Your Bank',
-        'content': '<p><img class=\'643\' alt=\'quityourbank\' src=\'/assets/images/2013/08/quityourbank.png\' width=\'700\' height=\'471\' /></p>\n',
+        'content': "<p><img  alt='quityourbank' src='/assets/images/2013/08/quityourbank.png' width='700' height='471' /></p> ",
         'tags': [
           'design',
           'javascript',
@@ -156,10 +201,6 @@ function getProjects() {
         ],
         'description': 'At the height of Occupy Wallstreet my girlfriend and I wanted to find a way to contribute and came up with QuitYourBank.org, a site to showcase solidarity support for alternative banking systems in opposition to the global banking cartels.'
       }
-    ],
-    'query': {
-      'ignore_sticky_posts': true,
-      'count': '200'
-    }
+    ]
   }
 }
